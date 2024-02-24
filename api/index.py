8b9f923332aededb880f991c3d9893e1f664e1b4
Kauan Sena <lkrs@protonmail.com>
2024-02-23 22:00:02 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,11 +9,11 @@ def home():
     return {"message": "Search engine API running", "engines online": {"DuckGo": ["endpoint": "/engines/duckgo/"]}, "status code": 200} 200
 
 @app.route('/engines')
-def engines(q):
+def engines():
     return {"search engines": {"DuckGo": ["server": "https://duckduckgo.com/", "service status": "online"], "Google": ["server": "https://google.com/", "service status": "offline"]}, "status code": 200}, 200
 
 @app.route('/engines/duckgo')
-def duckgo_info(q):
+def duckgo_info():
     return {"search engine": "DuckGo", "server": "https://duckduckgo.com/", "service status": "online", "status code": 200}, 200
     
 @app.route('/engines/duckgo/<q>')
@@ -22,7 +22,7 @@ def duckgo_engine(q):
     return results, status_code
     
 @app.route('/engines/google')
-def google_info(q):
+def google_info():
     return {"search engine": "Google", "server": "https://google.com/", "service status": "offline", "status code": 503}, 503
 
 @app.route('/engines/google/<q>')
