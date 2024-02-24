ac883c8d9a4d062361543809abc155b33f43b2f7
Kauan Sena <lkrs@protonmail.com>
2024-02-23 21:22:28 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from .engine import duckgo
 
 app = Flask(__name__)
 
+print(resultados_json)
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return {"message": "Search engine API running", "engines online": {"name": "DuckGo", "endpoint": "/engines/duckgo/"}}
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/engines/duckgo/<q>')
+def about(q):
+    results = duckgo(query=q)
+    response = json.dumps(results, ensure_ascii=False, indent=4)
+    return response
