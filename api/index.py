bc468bc8dce930b2af714b08133feb7f2ae715bb
Kauan Sena <lkrs@protonmail.com>
2024-02-23 21:35:02 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,7 +9,6 @@ def home():
     return {"message": "Search engine API running", "engines online": {"name": "DuckGo", "endpoint": "/engines/duckgo/"}}
 
 @app.route('/engines/duckgo/<q>')
-def about(q):
-    results = duckgo(query=q)
-    response = json.dumps(results, ensure_ascii=False, indent=4)
-    return response
+def duckgo(q):
+    results, status_code = duckgo(query=q)
+    return results, status_code
